8cce27b78a0987cc5bb3e27395d8a42db0bd1b86
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-01 15:25:09 -0600

    rest of starter folders/files added

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const path = require('path'); //importh path module
+const path = require('path'); //import path module
+const express = require('express'); //import express module
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3006;
 
 //take contents of public folder and make it into static assets
 app.use(express.static(path.join(__dirname, 'public'))); 
