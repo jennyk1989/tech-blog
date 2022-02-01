40f1a46ae2c26ba04586b8e8fb0b1879c97dc37d
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-01 15:30:27 -0600

    updated  server.js code

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const path = require('path'); //import path module
 const express = require('express'); //import express module
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars'); //import handlebars template engine
+const sequelize = require('./config/connection'); 
 
 const app = express();
 const PORT = process.env.PORT || 3006;
 
+//adding template engine
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 //take contents of public folder and make it into static assets
 app.use(express.static(path.join(__dirname, 'public'))); 
+
+sequelize.syn({ force: false}).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
