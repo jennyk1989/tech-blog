e7b3af269f6aa429925b5e0daf86fc08bf2a18df
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-06 21:44:46 -0600

    added all express routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,5 @@
+//create router as an Express module
+const router = require('express').Router();
+
+// export router module to be loaded by api/index.js
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,1 +1,6 @@
 //this file will contain all of the user-facing routes such as the homepage & login
+//create router as an Express module
+const router = require('express').Router();
+
+// export router module to be loaded by controllers/index.js
+module.exports = router;
