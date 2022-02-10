f2d6f12c4cd1d0178cf088b1bb3caf3ee5940f63
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-10 13:47:23 -0600

    User and post seeds added

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,1 +1,23 @@
 // seeds for some posts
+const { Post } = require('../models');
+
+const postSeeds = () => Post.bulkcreate([
+    {
+        title: 'Tech News Today',
+        post_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        user_id: 1
+    },
+    {
+        title: 'Tech in the Past',
+        post_text: 'Sit amet porttitor eget dolor morbi non arcu risus. A lacus vestibulum sed arcu non. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Tincidunt eget nullam non nisi. Et ultrices neque ornare aenean euismod elementum nisi.', 
+        user_id: 2
+    },
+    {
+        title: 'Tech in the Future',
+        post_text: 'Viverra aliquet eget sit amet tellus cras adipiscing enim. Mauris augue neque gravida in fermentum et sollicitudin ac. Consectetur adipiscing elit duis tristique sollicitudin nibh. Et malesuada fames ac turpis egestas sed.',       
+        user_id: 3
+    },
+
+]);
+
+module.exports = postSeeds;
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,1 +1,19 @@
 // seeds for adding some users
+const { User } = require('../models'); //import User model
+
+const userSeeds = () => User.bulkcreate([
+    {
+        username: 'JohnDoe',
+        password: 'johnpass'
+    },
+    {
+        username: 'MarySmith',
+        password: 'marypass'
+    },
+    {
+        username: 'JoeBlow',
+        password: 'joepass'
+    }
+]);
+
+module.exports = userSeeds;
