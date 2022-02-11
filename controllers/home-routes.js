032fbc5d7c1e3902284ba515f0454fc4fe979b65
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-11 14:00:50 -0600

    Update home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,19 @@
 //this file will contain all of the user-facing routes such as the homepage & login
+// import models
+const { User, Post, Comment } = require('../models');
 //create router as an Express module
 const router = require('express').Router();
+//connect to sequelize`
+const sequelize = require('../config/connection');
+
+// homepage rendering
+
+// after login page
+
+// signing up page
+
+// see post up close page
+
 
 // export router module to be loaded by controllers/index.js
 module.exports = router;
