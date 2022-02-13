5d83df3ebe31cfae8d0196ca421b2c76d86e2619
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-13 14:14:16 -0600

    main layout added to handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,7 @@
-//handlebar.js helpers here
+//handlebar.js helpers here (only need date helper for this app)
+module.exports = {
+    //format date into MM/DD/YYYY format via Date object
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear}`;
+    }
+};
