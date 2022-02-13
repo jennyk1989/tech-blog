0bbb3492075754ee65e251be548c57b029144f0b
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-12 18:09:38 -0600

    adding homepg routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,8 +6,38 @@ const router = require('express').Router();
 //connect to sequelize
 const sequelize = require('../config/connection');
 
-// homepage rendering
-
+// homepage rendering (user not logged in)
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post-text',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                } 
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(data => {
+        const posts = data.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
 // after login page
 
 // signing up page
