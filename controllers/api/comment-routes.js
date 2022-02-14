5f2834c5825090429cbf7300380e63a102883e67
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-13 21:35:26 -0600

    Update comment-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -6,10 +6,45 @@ const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // route to get all comments
-
+router.get('/', (req, res) => {
+    Comment.findAll({})
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 // route to create new comments 
-
+router.post('/', withAuth, (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        user_id: req.session.user_id //use user id from session
+    })
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+})
 // route to delete comments
-
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(data => {
+        if(!data) {
+            res.status(404).json({ message: 'No comment found with this id'});
+            return;
+        };
+        res.json(data); 
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+});
 // export router module to be loaded by api/index.js
 module.exports = router;
