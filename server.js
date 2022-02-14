e7a38db0fa5a50cf24f99608a72058b894bcddd3
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-14 13:59:32 -0600

    Update server.js

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-sequelize.sync({ force: false}).then(() => {
-    app.listen(process.env.PORT, () => console.log('Now listening'));
+app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}!`);
+    sequelize.syn({ force: false });
 });
