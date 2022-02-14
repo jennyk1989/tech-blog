dffde7fd3fd7c6cb2a3584b9733684639809b9c5
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-13 18:03:48 -0600

    login and logout routes/templates/js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,44 +6,103 @@ const router = require('express').Router();
 //connect to sequelize
 const sequelize = require('../config/connection');
 
-// route for homepage with all posts (user not logged in)
+// route for rendering all posts to the homepage
 router.get('/', (req, res) => {
     Post.findAll({
-        attributes: [
+        attributes: [ //Post table columns + created_at (from post-info.hbs)
             'id',
             'title',
             'post-text',
             'created_at'
         ],
-        include: [
+        include: [ //also wants to include Comments on the post and author of the post
             {
                 model: Comment,
+                // columns of the Comment table + created_at from post-info.hbs
                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
+                include: { //include the username of the user that posted the comment
                     model: User,
                     attributes: ['username']
                 } 
             },
-            {
+            { //include author of the post
                 model: User,
                 attributes: ['username']
             }
         ]
     })
     .then(data => {
+        // creates "posts" array which contains serialized post object
         const posts = data.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+        // render the homepage.hbs template
+        res.render('homepage', { 
+            posts, //pass this "posts" object into homepage.hbs template
+            //conditionally render login/logout links based on the user's session:
+            loggedIn: req.session.loggedIn 
+        });
     })
     .catch(err => {
+        console.log(err);
         res.status(500).json(err);
     })
 })
-// route for homepage with user logged in
 
-// route for showing sign up page
+// route to get a single post (used when user clicks on a post)
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+           id: req.params.id 
+        },
+        attributes: [ //Post table columns + created_at (from post-info.hbs)
+            'id',
+            'title',
+            'post-text',
+            'created_at'
+        ],
+        include: [ //also wants to include Comments on the post and author of the post
+            {
+                model: Comment,
+                // columns of the Comment table + created_at from post-info.hbs
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: { //include the username of the user that posted the comment
+                    model: User,
+                    attributes: ['username']
+                } 
+            },
+            { //include author of the post
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(data => {
+        if(!data) {
+            res.status(404).json({ message: 'No post found with this id'});
+            return;
+        }
 
-// route for page showing one post 
+        //? serialize the data
+        const post = data.get({ plain: true});
+        //? pass data to template using post object
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
 
+// route to check if user logged in 
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/'); //redirecting user to home page after user logs in
+        return;
+    };
+    res.render('login'); //render the login template
+});
 
 // export router module to be loaded by controllers/index.js
 module.exports = router;
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,1 +1,31 @@
-// response to login in button being click
+// response to login in button being clicked
+async function loginHandler(event) {
+    event.preventDefault();
+    // grab data from the login form and put in variables
+    const username = document.querySelector('#username-login'); //id from username form in login.hbs
+    const password = document.querySelector('#password-login'); //id from password form in login.hbs
+
+    //if user inputs username & password...
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            // need to POST the username and password from the login form to the server
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if(response.ok) {
+            document.location.replace('/dashboard'); // if sign up successful, go to the dashboard
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+
+//? listener for login button being clicked:
+document.querySelector('.login-form').addEventListener('submit', loginHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupHandler);
