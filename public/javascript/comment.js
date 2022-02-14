ac4065ec23995e8ce80226edf3fcb5da20d105fa
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-13 22:27:02 -0600

    comments js, hbs

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,1 +1,29 @@
 // response to add a comment
+async function commentHandler(event) {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('textarea["#comment-body"]').value.trim();
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if(comment_text) {
+        const response = await fetch ('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if(response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        };
+    };
+};
+
+document.querySelector('#comment-form').addEventListener('submit', commentHandler);
--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,1 +1,25 @@
 //response to delete button being clicked on a post
+async function deletePostHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+            post_id: id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (response.ok) {
+        document.location.replace('/dashboard'); //go to dash once post successfully deleted
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#delete-post-btn').addEventListener('click', deletePostHandler);
