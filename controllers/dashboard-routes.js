cc53b6a62dde71784574d8c57dea844a86c18388
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-13 20:29:14 -0600

    dashboard template and routes added

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,11 +5,101 @@ const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 const sequelize = require('../../config/connection');
 
-// get the posts for the dashboard (user logged in)
+// get the user's posts for the dashboard (user logged in)
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [ //Post table columns + created_at (from post-info.hbs)
+            'id',
+            'title',
+            'post-text',
+            'created_at'
+        ],
+        include: [ //also wants to include Comments on the post and author of the post
+            {
+                model: Comment,
+                // columns of the Comment table + created_at from post-info.hbs
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: { //include the username of the user that posted the comment
+                    model: User,
+                    attributes: ['username']
+                } 
+            },
+            { //include author of the post
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(data => {
+        // creates "posts" array which contains serialized post object
+        const posts = data.map(post => post.get({ plain: true }));
+        // render the dashboard.hbs template
+        res.render('dashboard', { 
+            posts, //pass this "posts" object into dashboard.hbs template
+            //conditionally render login/logout links based on the user's session:
+            loggedIn: true 
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
 
-// route for page for user to edit post
+// route for rendering page where user can edit post
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+           id: req.params.id 
+        },
+        attributes: [ //Post table columns + created_at (from post-info.hbs)
+            'id',
+            'title',
+            'post-text',
+            'created_at'
+        ],
+        include: [ //also wants to include Comments on the post and author of the post
+            {
+                model: Comment,
+                // columns of the Comment table + created_at from post-info.hbs
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: { //include the username of the user that posted the comment
+                    model: User,
+                    attributes: ['username']
+                } 
+            },
+            { //include author of the post
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(data => {
+        if(!data) {
+            res.status(404).json({ message: 'No post found with this id'});
+            return;
+        }
+
+        //? serialize the data
+        const post = data.get({ plain: true});
+        //? pass data to template using post object
+        res.render('edit-posts', {
+            post,
+            loggedIn: true
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
 
 // route for page where user adds a new post
-
+router.get('/newpost', (req, res) => {
+    res.render('new-posts');
+});
 // export router module to be loaded by controllers/index.js
 module.exports = router;
--- utils/auth.js
+++ utils/auth.js
@@ -1,1 +1,10 @@
 //auth. a user is loggin in
+const withAuth = (req, res, next) => {
+    if(!req.session.user_id) {
+        res.redirect('/login'); //redirect user to login if no session
+    } else {
+        next(); //calls next middleware fx
+    }
+};
+
+module.exports = withAuth;
