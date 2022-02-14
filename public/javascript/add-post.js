85ab7ddfdbb22f4e5e27c87cd3bc80950fab9e87
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-13 22:01:20 -0600

    edit & add posts js and templates

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,1 +1,25 @@
 // response to + New Post button being clicked
+async function newPostHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input["#post-title"]').value;
+    const post_text = document.querySelector('textarea["#post-text"]').value;
+
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            post_text
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if(response.ok) {
+        document.location.replace('/dashboard'); //load dashboard once new post added successfully
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#new-post-form').addEventListener('submit', newPostHandler);
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,1 +1,28 @@
 // response to edit
+async function editPostHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input["#post-title"]').value;
+    const post_text = document.querySelector('textarea["#post-text"]').value;
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT', //use PUT method since updating
+        body: JSON.stringify({
+            title,
+            post_text
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if(response.ok) {
+        document.location.replace('/dashboard'); //load dashboard once new post added successfully
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#edit-post-form').addEventListener('submit', editPostHandler);
